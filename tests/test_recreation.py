b89afaae79a32394187f1ca50e920cf1c7681b1e
davemfish <davemfish@gmail.com>
2023-12-13 15:10:01 -0500

    remove another test that we have skipped for years; it also seems redundant. #1366

--- tests/test_recreation.py
+++ tests/test_recreation.py
@@ -857,30 +857,6 @@ class RecreationRegressionTests(unittest.TestCase):
         utils._assert_vectors_equal(
             expected_grid_vector_path, out_grid_vector_path)
 
-    @unittest.skip("skipping to avoid remote server call (issue #3753)")
-    def test_no_grid_execute(self):
-        """Recreation execute on ungridded AOI."""
-        from natcap.invest.recreation import recmodel_client
-
-        args = {
-            'aoi_path': os.path.join(SAMPLE_DATA, 'andros_aoi.shp'),
-            'compute_regression': False,
-            'start_year': '2005',
-            'end_year': '2014',
-            'grid_aoi': False,
-            'results_suffix': '',
-            'workspace_dir': self.workspace_dir,
-        }
-
-        recmodel_client.execute(args)
-
-        expected_result_table = pandas.read_csv(os.path.join(
-            REGRESSION_DATA, 'expected_monthly_table_for_no_grid.csv'))
-        result_table = pandas.read_csv(
-            os.path.join(self.workspace_dir, 'monthly_table.csv'))
-        pandas.testing.assert_frame_equal(
-            expected_result_table, result_table, check_dtype=False)
-
     def test_predictor_id_too_long(self):
         """Recreation can validate predictor ID length."""
         from natcap.invest.recreation import recmodel_client
