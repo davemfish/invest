0413db0d503e85ad7621e668668b50275605cfa2
davemfish <davemfish@gmail.com>
2026-08-26 12:27:07 -0400

    update keywords test. #2663

--- tests/test_keywords.py
+++ tests/test_keywords.py
@@ -7,21 +7,21 @@ class TestKeywords(unittest.TestCase):
     """Test and validate characteristics of the keywords vocabulary."""
 
     def test_unique(self):
-        """Test values, aliases, and uuids are all unique."""
+        """Test values, aliases, and urls are all unique."""
         values = set()  # values and aliases together must be unique
-        uuids = set()
+        urls = set()
         for keyword in keywords.to_list():
             self.assertNotIn(
-                keyword.value, values,
-                msg=f'\n duplicate value {keyword.value} on {keyword}')
-            values.add(keyword.value)
+                keyword.name, values,
+                msg=f'\n duplicate value {keyword.name} on {keyword}')
+            values.add(keyword.name)
             for alias in keyword.aliases:
                 self.assertNotIn(
                     alias, values,
                     msg=f'\n duplicate alias {alias} on {keyword}')
                 values.add(alias)
-            if hasattr(keyword, 'uuid'):
+            if keyword.url:
                 self.assertNotIn(
-                    keyword.uuid, uuids,
-                    msg=f'\n duplicate uuid on {keyword}')
-                uuids.add(keyword.uuid)
+                    keyword.url, urls,
+                    msg=f'\n duplicate url on {keyword}')
+                urls.add(keyword.url)
